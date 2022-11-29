4d1b3950f320f0cd48db92e63dc1e4d7ec603976
shelarlahu24 <95674448+shelarlahu24@users.noreply.github.com>
2022-11-29 14:54:16 +0530

    .

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -7,6 +7,7 @@ import Loader from "../components/Loader"
 import { useDispatch, useSelector } from "react-redux"
 import { getUserDetails, updateUserProfile } from "../actions/userActions.js";
 import { listMyOrder } from '../actions/orderActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = () => {
 
@@ -31,7 +32,8 @@ const ProfileScreen = () => {
     if (!userInfo) {
       navigate('/login');
     } else {
-      if (!user.name) {
+      if (!user || !user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
         dispatch(getUserDetails('profile'))
         dispatch(listMyOrder())
       } else {
@@ -39,7 +41,7 @@ const ProfileScreen = () => {
         setEmail(user.email);
       }
     }
-  }, [dispatch, navigate, userInfo, user])
+  }, [dispatch, navigate, userInfo, user, success])
 
   const submitHandler = (e) => {
     e.preventDefault();
